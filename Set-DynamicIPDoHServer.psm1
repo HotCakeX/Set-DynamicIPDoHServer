76d71dfb35fce934e1b5f30b7542d0f28326905f
HotCakeX <118815227+HotCakeX@users.noreply.github.com>
2023-01-12 00:03:40 +0000

    Update Set-DynamicIPDoHServer.psm1

--- Set-DynamicIPDoHServer.psm1
+++ Set-DynamicIPDoHServer.psm1
@@ -179,6 +179,13 @@ Set-DnsClientServerAddress -InterfaceIndex $ActiveNetworkInterface.ifIndex -Rese
 
 
 
+# Enables "TLS_CHACHA20_POLY1305_SHA256" Cipher Suite for Windows 11, if necessary, because it's disabled by default
+# cURL will need that cipher suite to perform encrypted DNS query, it uses Windows Schannel
+if (-NOT ((Get-TlsCipherSuite).name -contains "TLS_CHACHA20_POLY1305_SHA256"))
+{Enable-TlsCipherSuite -Name "TLS_CHACHA20_POLY1305_SHA256"}
+
+
+
 
 
 # delete all other previous DoH settings for ALL Interface - Windows behavior in settings when changing DoH settings is to delete all DoH settings for the interface we are modifying 
